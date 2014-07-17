1b01f0499693093bd3312bee28c7e4b4f9180d58
Piotr <masourek@gmail.com>
2014-07-17 22:31:38 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from Point3d import *
+from Vector3d import *
+import random as r
+
+for i in range(10):
+    p = Point3d()
+    x, y, z = r.random()*100, r.random()*100, r.random()*100
+    vector = Vector3d(x, y, z)
+    p.setPosition(vector)
+    
+    x, y, z = r.random()*10, r.random()*10, r.random()*10
+    vector = Vector3d(x, y, z)
+    p.setVelocity(vector)
+    
+    print p
+
+point = Point3d()
+pos = Vector3d(10,10,10)
+vel = Vector3d(1,2,3)
+point.setPosition(pos)
+point.setVelocity(vel)
+
+dt = 0.01 # time delta
+for i in range(15):
+    pos = point.getPosition()
+    vel = point.getVelocity()
+    new_position = pos.derivate(vel, dt)
+    point.setPosition(new_position)
+    
+    print point
+    #vec3d
+    
+
