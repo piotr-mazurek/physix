612264e5fd8f7390cf29547ced950ecf4d81c3e8
matlobocki <mateusz.lobocki@stxnext.pl>
2014-07-18 09:30:08 +0200

    pep8 fixes

--- Vector3d.py
+++ Vector3d.py
@@ -1,42 +1,40 @@
 class Vector3d():
     x, y, z = (0.0, 0.0, 0.0)
-        
+
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-    
+
     def setValues(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-        
+
     def getX(self):
         return self.x
-    
+
     def getY(self):
         return self.y
-    
+
     def getZ(self):
         return self.z
-    
+
     def __mul__(self, vector):
         x = self.x * vector.getX()
         y = self.y * vector.getY()
         z = self.z * vector.getZ()
-        return Vector3d(x,y,z)
-    
+        return Vector3d(x, y, z)
+
     def derivate(self, vector, dt):
         x = self.x + dt * vector.getX()
         y = self.y + dt * vector.getY()
         z = self.z + dt * vector.getZ()
-        return Vector3d(x,y,z)
-    
-    
+        return Vector3d(x, y, z)
+
     def __repr__(self):
         return "[x:%.2f, y:%.2f, z:%.2f]" % (
             self.getX(),
             self.getY(),
             self.getZ(),
         )
-
--- main.py
+++ main.py
@@ -7,27 +7,25 @@ for i in range(10):
     x, y, z = r.random()*100, r.random()*100, r.random()*100
     vector = Vector3d(x, y, z)
     p.setPosition(vector)
-    
+
     x, y, z = r.random()*10, r.random()*10, r.random()*10
     vector = Vector3d(x, y, z)
     p.setVelocity(vector)
-    
+
     print p
 
 point = Point3d()
-pos = Vector3d(10,10,10)
-vel = Vector3d(1,2,3)
+pos = Vector3d(10, 10, 10)
+vel = Vector3d(1, 2, 3)
 point.setPosition(pos)
 point.setVelocity(vel)
 
-dt = 0.01 # time delta
+dt = 0.01
 for i in range(15):
     pos = point.getPosition()
     vel = point.getVelocity()
     new_position = pos.derivate(vel, dt)
     point.setPosition(new_position)
-    
+
     print point
     #vec3d
-    
-
