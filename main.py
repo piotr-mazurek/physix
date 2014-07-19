ca930c0a67d076403399bbbc20eec33214af89d4
Piotr <masourek@gmail.com>
2014-07-19 23:11:04 +0200

    removing unused files

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from Point3d import *
-from Vector3d import *
+from Point3D import *
+from Vector3D import *
 import random as r
 
 for i in range(10):
-    p = Point3d()
+    p = Point3D()
     x, y, z = r.random()*100, r.random()*100, r.random()*100
-    vector = Vector3d(x, y, z)
+    vector = Vector3D(x, y, z)
     p.setPosition(vector)
 
     x, y, z = r.random()*10, r.random()*10, r.random()*10
-    vector = Vector3d(x, y, z)
+    vector = Vector3D(x, y, z)
     p.setVelocity(vector)
 
     print p
 
-point = Point3d()
-pos = Vector3d(10, 10, 10)
-vel = Vector3d(1, 2, 3)
+point = Point3D()
+pos = Vector3D(10, 10, 10)
+vel = Vector3D(1, 2, 3)
 point.setPosition(pos)
 point.setVelocity(vel)
 
@@ -28,4 +28,4 @@ for i in range(15):
     point.setPosition(new_position)
 
     print point
-    #vec3d
+    #vec3D
